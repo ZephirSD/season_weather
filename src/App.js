4f380e9d477ee6d8fe7fa11490349e0127121392
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-07-10 20:12:24 +0400

    Insertion de rendu 3D sur `App.js`

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import  './style/style.scss';
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+// import { Html } from "@react-three/drei";
+import "./style/style.scss";
+import Model from "./composant/modules/Lanscape";
 
 function App() {
   return (
     <>
+      <Canvas camera={{position: [15,8,-5]}} style={{ width: "100%", height: "100vh" }}>
+        <ambientLight />
+          <Suspense>
+              <Model />
+          </Suspense>
+      </Canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
