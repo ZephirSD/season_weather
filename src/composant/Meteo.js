2a057786c54c221b5192df95fbe0d388c281314c
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-07-16 13:10:27 +0400

    Fetch Map pour les indicateurs météo

--- src/composant/Meteo.js
+++ src/composant/Meteo.js
@@ -5,22 +5,23 @@ import "swiper/css";
 import "swiper/css/pagination";
 import direction from "../assets/icones/direction_icon.png";
 import water from "../assets/icones/water_icon.png";
-import  { Humidite, Temperature, Vent } from "./modules/Indicateur";
+import wave from "../assets/icones/wave_icon.png";
+import { Humidite, Pression, Temperature, Vent } from "./modules/Indicateur";
 
 function Meteo() {
-  const [data, setData] = useState({});
-  const meteoAPI = async () => {
-    const api = await fetch(
+  const [data, setData] = useState([]);
+  const meteoAPI = () => {
+    fetch(
       "http://api.openweathermap.org/data/2.5/onecall?lat=-21.115141&lon=55.536384&appid=c8bf235a8a58fb21fff7b54152bbd9cd&units=metric&exclude=hourly,alerts,current"
-    );
-    const response = await api.json();
-    setData(response.daily[0]);
+    )
+      .then((reponse) => reponse.json())
+      .then((value) => setData(value.daily))
+      .catch((error) => console.log("error", error));
   };
-//   console.log(data);
+  // console.log(data);
   useEffect(() => {
-      meteoAPI();
-    //   console.log(data);
-  },[])
+    meteoAPI();
+  }, []);
   return (
     <>
       <header>
@@ -32,15 +33,26 @@ function Meteo() {
               clickable: true,
             }}
           >
-            <SwiperSlide>
-              <Temperature indic={data.temp.day} weather={data.weather.main}/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Vent img={direction} indic={data.wind_speed} degre={data.wind_deg}/>
-            </SwiperSlide>
-            <SwiperSlide>
-              <Humidite img={water} indic={data.humidity}/>
-            </SwiperSlide>
+            {data.map((res, index) =>
+              index === 0 ? (
+                <><SwiperSlide>
+                  <Temperature
+                    indic={res.temp.day}
+                    weather={res.weather}
+                    key={index} />
+                </SwiperSlide><SwiperSlide>
+                    <Vent img={direction} indic={res.wind_speed} degre={res.wind_deg} />
+                  </SwiperSlide><SwiperSlide>
+                    <Humidite img={water} indic={res.humidity} />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <Pression img={wave} indic={res.pressure}/>
+                  </SwiperSlide>
+                  </>
+              ) : (
+                <></>
+              )
+            )}
           </Swiper>
         </div>
       </header>
