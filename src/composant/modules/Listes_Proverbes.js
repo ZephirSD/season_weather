dcb83f755c55a39d0189239570affc1d831218c6
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-07-16 20:34:11 +0400

    Création des tables `listes_proverbes_jour` & `listes_proverbes_nuit`

---
+++ src/composant/modules/Listes_Proverbes.js
@@ -0,0 +1,141 @@
+export const listes_proverbes_jour = 
+[
+    {
+        id: 1,
+        proverbes: "Plus vous devenez silencieux , plus vous pouvez entendre"
+    
+    },
+    {
+        id: 2,
+        proverbes: "C’est incroyable comme la vie devient belle quand on prend un moment pour s’arrêter."    
+    },
+    {
+        id: 3,
+        proverbes: "Rien ne me fait sentir plus libre que de rester assis dans la nature pour respirer."    
+    },
+    {
+        id: 4,
+        proverbes: "La méditation me rappelle que mon corps et mon esprit ne font qu’un avec la nature."    
+    },
+    {
+        id: 5,
+        proverbes: "Le monde parle à ceux qui ont l’esprit tranquille."    
+    },
+    {
+        id: 6,
+        proverbes: "Les gens avisés ne luttent pas pour la gloire et la richesse. Ils se battent pour l’amour et la compassion."    
+    },
+    {
+        id: 7,
+        proverbes: "Je n’ai pas le temps d’écouter les bruits du monde. Je suis trop occupé à vire dans le moment présent."    
+    },
+    {
+        id: 8,
+        proverbes: "Rien ne rend mes matinées plus lumineuses que 5 millions de salutations au soleil."    
+    },
+    {
+        id: 9,
+        proverbes: "Toutes les grands pensées sont conçues en marchant."    
+    },
+    {
+        id: 10,
+        proverbes: "Aujourd’hui, rien ne peut me toucher. Mon esprit Zen est invincible."    
+    },
+    {
+        id: 11,
+        proverbes: "Peu importe ce que les autres pensent de vous. Tout ce qui compte, c’est ce que vous pensez de vous."    
+    },
+    {
+        id: 12,
+        proverbes: "Ne sous-estimez jamais votre propre capacité à changer."    
+    },
+    {
+        id: 13,
+        proverbes: "Même dans les moments difficiles, je suis toujours rempli de gratitude."    
+    },
+    {
+        id: 14,
+        proverbes: "Changez votre façon de voir les choses et le monde entier changera avec vous."    
+    },
+    {
+        id: 16,
+        proverbes: "Méditer sur la plage, c’est laisser votre esprit embrasser les vagues."    
+    },
+    {
+        id: 17,
+        proverbes: "La paix vient de l’intérieur."    
+    }
+];
+
+export const listes_proverbes_nuit = 
+[
+    {
+        id: 1,
+        proverbes: "L'indifférence est le sommeil de l'âme."
+    },
+    {
+        id: 2,
+        proverbes: "Rien ne s'oublie, l'âme dans le sommeil médite et rumine son passé"
+    },
+    {
+        id: 3,
+        proverbes: "Le sommeil est le seul ami qui ne vient pas quand on l'appelle"
+    },
+    {
+        id: 4,
+        proverbes: "Le sommeil est un amour perdu"
+    },
+    {
+        id: 5,
+        proverbes: "Le sommeil permet à notre âme de prendre des heures de récréation"
+    },
+    {
+        id: 6,
+        proverbes: "Le plus beau sommeil ne vaut pas le moment où l'on se réveille"
+    },
+    {
+        id: 7,
+        proverbes: "S'il fait jour, vive le soleil ! S'il fait nuit, vive le sommeil"
+    },
+    {
+        id: 8,
+        proverbes: "Bien plus que le temps, c'est le sommeil qui est l'antidote du chagrin"
+    },
+    {
+        id: 9,
+        proverbes: "Béni soit celui qui inventa la sommeil"
+    },
+    {
+        id: 10,
+        proverbes: "Comme une journée bien remplie nous donne un bon sommeil."
+    },
+    {
+        id: 11,
+        proverbes: "Rêve d'une nuit d'été, sommeil perturbé. Matin troublé, songes révélé"
+    },
+    {
+        id: 12,
+        proverbes: "Mieux vaut un bon sommeil qu'un bon lit"
+    },
+    {
+        id: 13,
+        proverbes: "On ne peut pas dompter les rêveurs"
+    },
+    {
+        id: 14,
+        proverbes: "Les rêves ont été crées pour qu'on ne s'ennuie pas pendant le sommeil"
+    },
+    {
+        id: 15,
+        proverbes: "L'ennui réveille l'appétit et trouve le sommeil pour consolation"
+    },
+    {
+        id: 16,
+        proverbes: "Le rire et le somméil sont les meilleurs remèdes au monde"
+    },
+    {
+        id: 17,
+        proverbes: "Trop de sommeil est une remède qui ouvre la porte à la pauvreté spirituelle de l'âme"
+    },
+];
+
