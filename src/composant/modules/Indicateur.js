dcdec3cdc1f72a56bb73165bf93d4cf19d7968de
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-07-16 13:09:38 +0400

    Ajout de composant d'indicateurs météo et changement de système pour la récupérations des props

--- src/composant/modules/Indicateur.js
+++ src/composant/modules/Indicateur.js
@@ -5,18 +5,24 @@ import rain from "../../assets/icones/rain_icon.png";
 
 function Temperature({indic,weather}) {
   const [changeWeather,setChangeWeather] = useState();
-  const functionWeather = () => {
-    if(weather === 'Clear'){
-      setChangeWeather(sun);
-      console.log('clear');
+  const [indicState,setIndicState] = useState();
+  const functionWeather = async () => {
+    let mainWeather = await weather;
+    if(mainWeather !== undefined){
+      mainWeather.map((weat) => (
+        (weat.main) === 'Rain' ?  setChangeWeather(rain): (weat.main) === 'Clear' ? setChangeWeather(sun) : <></>
+      ))
     }
-    else if(weather === 'Rain'){
-      setChangeWeather(rain);
-      console.log("rain");
+  }
+  const requestIndic = async () => {
+    let resultatIndic = await indic;
+    if(resultatIndic !== undefined){
+      setIndicState(resultatIndic);
     }
   }
   useEffect(() => {
     functionWeather();
+    requestIndic();
   })
   return (
     <>
@@ -25,7 +31,7 @@ function Temperature({indic,weather}) {
             <img src={changeWeather} alt="meteo" className='image-meteo'/>
         </div>
         <div className='indic-meteo' style={indic > 25 ? {color:'#DC6969'} : {color:'#6296D5'}}>
-            {`${Math.round(indic)}°C`}
+            {`${Math.round(indicState)}°C`}
         </div>
       </div>
     </>
@@ -62,4 +68,19 @@ function Humidite({img,indic}) {
   )
 }
 
-export {Temperature,Vent,Humidite}
+function Pression({img,indic}) {
+  return (
+    <>
+      <div className='grid-meteo'>
+        <div className='icon-box'>
+            <img src={img} alt="vent" className='image-meteo'/>
+        </div>
+        <div className='indic-meteo'>
+            {`${indic} hPa`}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export {Temperature,Vent,Humidite,Pression}
