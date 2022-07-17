d254a8e5e4b154ef6c45db1a634e627fd7e46ae1
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-07-17 10:56:46 +0400

    Ajout du composant `Proverbes` et création de fonction pour avoir l'heure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 import { Html } from "@react-three/drei";
 import "./style/style.scss";
 // import Model from "./composant/modules/Lanscape";
 import Fantasy from "./composant/modules/Fantasy";
-import Meteo from "./composant/Meteo";
+// import Meteo from "./composant/Meteo";
+import Proverbes from "./composant/Proverbes";
 
 function App() {
+  const [dateState, setDateState] = useState(0);
+  let date = new Date();
+  let hoursDisplay = date.getHours();
+  useEffect(() =>{
+      setInterval(() =>{
+          setDateState(hoursDisplay);
+      },1000)
+  })
   return (
     <>
       <Canvas flat={true} shadows={true} camera={{position: [240,120,-180]}} style={{ width: "100%", height: "100vh" }}>
@@ -15,7 +24,8 @@ function App() {
           <Suspense fallback={null}>
               <Fantasy />
               <Html fullscreen>
-                <Meteo/>
+                {/* <Meteo heuresDay={dateState}/> */}
+                <Proverbes heuresDay={dateState}/>
               </Html>
           </Suspense>
       </Canvas>
