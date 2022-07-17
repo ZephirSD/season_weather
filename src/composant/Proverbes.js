f949b3ef018d79e3cd709c952148610d9ddc7803
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-07-17 10:58:05 +0400

    Ajout du props `heuresDay` pour modifier les styles

--- src/composant/Proverbes.js
+++ src/composant/Proverbes.js
@@ -1,27 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { listes_proverbes_jour, listes_proverbes_nuit } from './modules/Listes_Proverbes'
 
-function Proverbes() {
-    const [dateState, setDateState] = useState();
-    let date = new Date();
-    let hoursDisplay = date.getHours();
-    useEffect(() =>{
-        setInterval(() =>{
-            setDateState(hoursDisplay);
-        },1000)
-    })
+function Proverbes({heuresDay}) {
   return (
     <>
-        <footer>
+        <footer style={(heuresDay > 18) ? {backgroundColor: 'var(--background-indic-night)', color: 'white'} : (heuresDay > 7) ? {backgroundColor: 'var(--background-indic)', color: 'black'} : {backgroundColor: 'var(--background-indic)', color: 'black'}}>
             {
-                (dateState > 19 && dateState < 7) ? 
+                (heuresDay > 18) ? 
                 listes_proverbes_nuit.map((listes,index) => (
                     index === 0 ? (<span key={listes.id}>{listes.proverbes}</span>) : (<></>)
                 ))
-                : 
-                listes_proverbes_jour.map((listes,index) => (
-                    index === 0 ? (<span key={listes.id}>{listes.proverbes}</span>) : (<></>)
-                ))
+                : (heuresDay > 7) ?
+                listes_proverbes_jour.map((listes2,index2) => (
+                    index2 === 0 ? (<span key={listes2.id}>{listes2.proverbes}</span>) : (<></>)
+                )):
+                <></>
             }
         </footer>
     </>
