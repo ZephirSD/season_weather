45b05eacaf02823d2afda153b5e26e7fc0408f82
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-07-27 12:04:15 +0400

    Modification de la structure du code

--- src/composant/Proverbes.js
+++ src/composant/Proverbes.js
@@ -1,24 +1,43 @@
-import React from 'react'
-import { listes_proverbes_jour, listes_proverbes_nuit } from './modules/Listes_Proverbes'
+import React from "react";
+import { listes_proverbes_jour, listes_proverbes_nuit } from "./modules/Listes_Proverbes";
 
-function Proverbes({heuresDay}) {
+function Proverbes({ heuresDay, dayMonths }) {
   return (
     <>
-        <footer style={(heuresDay > 18) ? {backgroundColor: 'var(--background-indic-night)', color: 'white'} : (heuresDay > 7) ? {backgroundColor: 'var(--background-indic)', color: 'black'} : {backgroundColor: 'var(--background-indic)', color: 'black'}}>
-            {
-                (heuresDay > 18) ? 
-                listes_proverbes_nuit.map((listes,index) => (
-                    index === 0 ? (<span key={listes.id}>{listes.proverbes}</span>) : (<></>)
-                ))
-                : (heuresDay > 7) ?
-                listes_proverbes_jour.map((listes2,index2) => (
-                    index2 === 0 ? (<span key={listes2.id}>{listes2.proverbes}</span>) : (<></>)
-                )):
-                <></>
-            }
-        </footer>
+      <footer
+        style={
+          heuresDay >= 18
+            ? {
+                backgroundColor: "var(--background-indic-night)",
+                color: "white",
+              }
+            : heuresDay >= 7
+            ? { backgroundColor: "var(--background-indic)", color: "black" }
+            : { backgroundColor: "var(--background-indic)", color: "black" }
+        }
+      >
+        {heuresDay >= 18 ? (
+          listes_proverbes_nuit.map((listes) =>
+            listes.id === dayMonths ? (
+              <span key={listes.id}>{listes.proverbes}</span>
+            ) : (
+              <></>
+            )
+          )
+        ) : heuresDay >= 7 ? (
+          listes_proverbes_jour.map((listes2) =>
+            listes2.id === dayMonths ? (
+              <span key={listes2.id}>{listes2.proverbes}</span>
+            ) : (
+              <></>
+            )
+          )
+        ) : (
+          <></>
+        )}
+      </footer>
     </>
-  )
+  );
 }
 
-export default Proverbes
+export default Proverbes;
